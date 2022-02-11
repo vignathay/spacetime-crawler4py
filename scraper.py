e29ab0385c301a5076036c3dcef27dbc086368ed
Alekhya <alekhyapyla27@gmail.com>
2022-02-10 18:21:22 -0800

    Update scraper.py
    Added taps

--- scraper.py
+++ scraper.py
@@ -102,6 +102,10 @@ def is_valid(url):
             return False
         if 'evoke.ics.uci.edu' in parsed.hostname and 'replytocom' in url:
             return False
+        if 'swiki.ics.uci.edu' in parsed.hostname:
+            return False
+        if 'sli.ics.uci.edu' in parsed.hostname and 'download' in url:
+            return False
         if re.match(r".*\.(css|js|bmp|gif|jpe?g|ico" + r"|png|tiff?|mid|mp2|mp3|mp4" + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|ppsx" + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names" + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso" + r"|epub|dll|cnf|tgz|sha1|tar.gz" + r"|thmx|mso|arff|rtf|jar|csv" + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
             return False
         if re.match(r".*\.(css|js|bmp|gif|jpe?g|ico" + r"|png|tiff?|mid|mp2|mp3|mp4" + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|ppsx" + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names" + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso" + r"|epub|dll|cnf|tgz|sha1|tar.gz" + r"|thmx|mso|arff|rtf|jar|csv" + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.query.lower()):
