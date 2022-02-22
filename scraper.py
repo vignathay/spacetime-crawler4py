5bd5469ae46f2e9a9332f70bc2f1d5446cbc033f
Alekhya <alekhyapyla27@gmail.com>
2022-02-22 13:04:41 -0800

    Trap Examples

--- scraper.py
+++ scraper.py
@@ -166,29 +166,29 @@ def is_valid(url):
 
         if parsed.scheme not in set(["http", "https"]):
             return False
-        if 'ical=' in url:  # Downloding calendar file
-            return False
+        if 'ical=' in url:  # Downloding calendar file 
+            return False    #https://wics.ics.uci.edu/event/fall-2021-week-4-resume-workshop/?ical=1
         if re.search(r"pdf", parsed.path.lower()):  # pdf file
-            return False
+            return False    #https://www.informatics.uci.edu/files/pdf/InformaticsBrochure-March2018
         if 'wics.ics.uci.edu' in parsed.hostname and (re.search('/events', url) or 'eventDate' in url):  # events
-            return False
+            return False        #https://wics.ics.uci.edu/event/fall-2021-week-4-resume-workshop/?ical=1
         if 'grape.ics.uci.edu' in parsed.hostname and '/wiki/' in url:
-            return False
+            return False        #https://grape.ics.uci.edu/wiki/asterix/wiki/WikiStart #https://grape.ics.uci.edu/wiki/public/wiki/cs222p-2018-fall?action=diff&version=59
         if 'mt-live.ics.uci.edu' in parsed.hostname and ('/events/' in url or 'eventDate' in url):
-            return False
+            return False        #https://mt-live.ics.uci.edu/event/2021-southern-california-software-engineering-symposium/?ical=1 #https://mt-live.ics.uci.edu/event/2021-southern-california-software-engineering-symposium
         if 'mt-live.ics.uci.edu' in parsed.hostname and 'people' in parsed.path.lower():
-            return False
+            return False        #https://mt-live.ics.uci.edu/people/?filter%5Boffices_ics%5D%5B0%5D=1082&filter%5Boffices_ics%5D%5B1%5D=1083&filter%5Boffices_ics%5D%5B2%5D=1077&filter%5Bemployee_type%5D%5B0%5D=783&filter%5Bemployee_type%5D%5B1%5D=1095&filter%5Bemployee_type%5D%5B3%5D=1094&filter%5Bresearch_areas_ics%5D%5B0%5D=1110&filter%5Bresearch_areas_ics%5D%5B2%5D=1114&filter%5Bresearch_areas_ics%5D%5B4%5D=1119&filter%5Bresearch_areas_ics%5D%5B5%5D=1880&filter%5Bresearch_areas_ics%5D%5B6%5D=1873&filter%5Bresearch_areas_ics%5D%5B7%5D=1120&filter%5Bresearch_areas_ics%5D%5B8%5D=1123&filter%5Bresearch_areas_ics%5D%5B9%5D=1132&filter%5Bresearch_areas_ics%5D%5B10%5D=1127&filter%5Bresearch_areas_ics%5D%5B11%5D=1891&filter%5Bresearch_areas_ics%5D%5B12%5D=1124&filter%5Bresearch_areas_ics%5D%5B13%5D=1900&filter%5Bresearch_areas_ics%5D%5B14%5D=1112&filter%5Bresearch_areas_ics%5D%5B15%5D=1882&filter%5Binstitutes_centers%5D%5B1%5D=1074&filter%5Binstitutes_centers%5D%5B2%5D=1072
         if 'archive.ics.uci.edu' in parsed.hostname and '/ml/dataset' in url:  # datasets
-            return False
+            return False #http://archive.ics.uci.edu/ml/datasets/Dodgers+Loop+Sensor
         if 'cbcl.ics.uci.edu' in parsed.hostname and ('do=' in url or '/data' in url or '/contact' in url):
-            return False
+            return False  #https://cbcl.ics.uci.edu/doku.php/software?do=diff&rev2%5B0%5D=1397844162&rev2%5B1%5D=1397844231&difftype=sidebyside
         if 'evoke.ics.uci.edu' in parsed.hostname and (
                 'replytocom' in url or 'comment' in url):  # comment threads and replies
-            return False
+            return False  #https://evoke.ics.uci.edu/about/values-in-design/?replytocom=106076
         if 'swiki.ics.uci.edu' in parsed.hostname:
-            return False
+            return False  #https://swiki.ics.uci.edu/doku.php/hardware:laptops?idx=hardware%3Astorage
         if 'sli.ics.uci.edu' in parsed.hostname and 'download' in url:  # download dataset
-            return False
+            return False  #http://sli.ics.uci.edu/Classes-2008F/Main?action=download&upname=Global_Puzzle
         if re.match(
                 r".*\.(css|js|bmp|gif|jpe?g|ico" + r"|png|tiff?|mid|mp2|mp3|mp4" + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf|ppsx" + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names" + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso" + r"|epub|dll|cnf|tgz|sha1|tar.gz" + r"|thmx|mso|arff|rtf|jar|csv" + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$",
                 parsed.path.lower()):
